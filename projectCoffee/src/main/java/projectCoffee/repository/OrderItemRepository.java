b2f8b91e39a9684c915271edf201b3e7bdb2d929
enfldia <139440376+enfldia@users.noreply.github.com>
2023-12-07 15:33:47 +0900

    Delete projectCoffee directory

--- projectCoffee/src/main/java/projectCoffee/repository/OrderItemRepository.java
+++
@@ -1,7 +0,0 @@
-package projectCoffee.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import projectCoffee.entity.OrderItem;
-
-public interface OrderItemRepository extends JpaRepository<OrderItem,Long> {
-}
