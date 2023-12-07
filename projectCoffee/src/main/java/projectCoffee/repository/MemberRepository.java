dbc1932adc55b0de4763e39f59ad8a97eda2a26a
enfldia <139440376+enfldia@users.noreply.github.com>
2023-12-07 15:56:09 +0900

    Delete projectCoffee directory

--- projectCoffee/src/main/java/projectCoffee/repository/MemberRepository.java
+++
@@ -1,9 +0,0 @@
-package projectCoffee.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import projectCoffee.entity.Member;
-
-public interface MemberRepository extends JpaRepository<Member, Long> {
-
-    Member findByEmail(String email);
-}
