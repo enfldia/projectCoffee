e7dfd8f6ae8dbdc89f9df3c0a87b0a1f787e30d5
ddu567 <shbea0503@naver.com>
2023-12-11 17:05:50 +0900

    1211
    - 로그인 오류 수정
    - 코드 정리

--- projectCoffee/src/main/java/projectCoffee/config/SecurityConfig.java
+++ projectCoffee/src/main/java/projectCoffee/config/SecurityConfig.java
@@ -29,13 +29,10 @@ public class SecurityConfig {
                 // oauth2Login
                 .and()
                 .oauth2Login()
-                // google
                 .loginPage("/members/login") //로그인이 필요한데 로그인을 하지 않았다면 이동할 uri 설정
                 .defaultSuccessUrl("/") //OAuth 구글 로그인이 성공하면 이동할 uri 설정
                 .userInfoEndpoint()//로그인 완료 후 회원 정보 받기
                 .userService(oAuth2MemberService)//로그인 후 받아온 유저 정보 처리
-                // naver
-                // kakao
                 // logout
                 .and()
                 .and()
@@ -63,24 +60,6 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
-
-    // OAuth2
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        return http
-//                .httpBasic().disable()
-//                .csrf().disable()
-//                .cors().and()
-//                .authorizeRequests()
-//                .requestMatchers(new AntPathRequestMatcher("/members/**")).authenticated()
-//                .anyRequest().permitAll() //나머지 uri는 모든 접근 허용
-//                .and().oauth2Login()
-//                .loginPage("/members/login") //로그인이 필요한데 로그인을 하지 않았다면 이동할 uri 설정
-//                .defaultSuccessUrl("/") //OAuth 구글 로그인이 성공하면 이동할 uri 설정
-//                .userInfoEndpoint()//로그인 완료 후 회원 정보 받기
-//                .userService(oAuth2MemberService).and().and().build(); //로그인 후 받아온 유저 정보 처리
-//    }
 
 
 }
