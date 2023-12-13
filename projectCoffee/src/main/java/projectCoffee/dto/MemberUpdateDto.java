9a01eaaf5b70fc0d5ae5f530949b0d706d264191
ddu567 <139440367+ddu567@users.noreply.github.com>
2023-12-13 17:43:58 +0900

    1213 Member (#16)
    - 회원정보수정 구현
    - 로그인 페이지 css 수정중

--- projectCoffee/src/main/java/projectCoffee/dto/MemberUpdateDto.java
+++ projectCoffee/src/main/java/projectCoffee/dto/MemberUpdateDto.java
@@ -20,13 +20,6 @@ public class MemberUpdateDto {
     @Email
     private String email;
 
-    @NotEmpty
-    @Pattern(regexp="(?=.*[0-9])(?=.*[a-z])(?=.*\\W)(?=\\S+$).{8,20}", message = "비밀번호는 숫자, 특수기호가 적어도 1개 이상씩 포함된 8자 ~ 20자의 비밀번호여야 합니다.")
-    private String password;
-
-    @NotEmpty(message = "비밀번호를 입력해주세요.")
-    private String password_confirm;
-
     @NotBlank
     @Pattern(regexp = "^[0-9]*$", message = "정수만 가능합니다.")
     @Size(min = 10, max = 11, message = "다시 한 번 확인해주세요.")
