4d007518953cdb3a1730ad4c30f7e60e1e3f5fcb
enfldia <enfldia12@naver.com>
2023-12-13 17:36:39 +0900

    상품 삭제 구현.
    상품 생성, 수정, 삭제 후 메인화면으로 이동 되던 걸,
    상품 관리 페이지로 이동되도록 수정
    
    상품 삭제 버튼 위치, 색상 변경

--- projectCoffee/src/main/java/projectCoffee/controller/ItemController.java
+++ projectCoffee/src/main/java/projectCoffee/controller/ItemController.java
@@ -34,7 +34,7 @@ public class ItemController {
     @PostMapping(value = "/admin/item/new")
     public String itemNew(@Valid ItemFormDto itemFormDto, BindingResult bindingResult,
                           Model model, @RequestParam("itemImgFile") List<MultipartFile>
-                          itemImgFileList){
+                          itemImgFileList, Optional<Integer> page,ItemSearchDto itemSearchDto){
 
         if(bindingResult.hasErrors()){
             return "item/itemForm";
@@ -51,8 +51,19 @@ public class ItemController {
             model.addAttribute("errorMessage", "상품 등록 중 에러가 발생하였습니다.");
                     return "item/itemForm";
         }
+        Pageable pageable = PageRequest.of(page.isPresent() ? page.get() : 0, 3);//
+        // PageRequest - Data jpa에서 사용하는 페이지 요청객체
+        // of() 메서드를 사용하여 페이지 번화와 페이지당 항목수 지정하여 페이지 요청 정보생성
+        // 0 은 url 경로에서 받아온 페이지 번호를 확인하고 , 값이 없으면 0 = 보고 있던 페이지가 없으면 첫번째 페이지를 보여줌
+        // 3 은 한 페이지 당 보여줄 항목수
 
-        return "redirect:http://localhost:8080/admin/items";
+
+        Page<Item> items = itemService.getAdminItemPage(itemSearchDto, pageable);
+        //itemSearchDto를 사용하여 페이지 네이션 된 테이터를 조회
+        model.addAttribute("items", items); //조회된 페이지 네이션된 데이터를 모델에 추가
+        model.addAttribute("itemSearchDto", itemSearchDto); //검색 조건 모델에 추가
+        model.addAttribute("maxPage", 5);
+        return "item/itemMng";
     }
 
     //상품 수정하기
@@ -70,16 +81,29 @@ public class ItemController {
     }
 
     @PostMapping("/admin/item/delete/{itemId}")
-    public String itemDelete(@PathVariable("itemId") Long itemId){
+    public String itemDelete(@PathVariable("itemId") Long itemId,Optional<Integer> page, ItemSearchDto itemSearchDto, Model model){
         itemService.deleteItem(itemId);
-        return "redirect:http://localhost:8080/admin/items";
+        Pageable pageable = PageRequest.of(page.isPresent() ? page.get() : 0, 3);//
+        // PageRequest - Data jpa에서 사용하는 페이지 요청객체
+        // of() 메서드를 사용하여 페이지 번화와 페이지당 항목수 지정하여 페이지 요청 정보생성
+        // 0 은 url 경로에서 받아온 페이지 번호를 확인하고 , 값이 없으면 0 = 보고 있던 페이지가 없으면 첫번째 페이지를 보여줌
+        // 3 은 한 페이지 당 보여줄 항목수
+
+
+        Page<Item> items = itemService.getAdminItemPage(itemSearchDto, pageable);
+        //itemSearchDto를 사용하여 페이지 네이션 된 테이터를 조회
+        model.addAttribute("items", items); //조회된 페이지 네이션된 데이터를 모델에 추가
+        model.addAttribute("itemSearchDto", itemSearchDto); //검색 조건 모델에 추가
+        model.addAttribute("maxPage", 5);
+        return "item/itemMng";
     }
 
 
 
     @PostMapping(value = "/admin/item/{itemId}")
     public String itemUpdate(@Valid ItemFormDto itemFormDto, BindingResult bindingResult,
-                             @RequestParam("itemImgFile") List<MultipartFile> itemImgFileList, Model model){
+                             @RequestParam("itemImgFile") List<MultipartFile> itemImgFileList,
+                             Optional<Integer> page,ItemSearchDto itemSearchDto, Model model){
         if(bindingResult.hasErrors()){
             return "item/itemForm";
         }
@@ -94,10 +118,22 @@ public class ItemController {
             model.addAttribute("errorMessage", "상품 수정 중 에러가 발생하였습니다.");
             return "item/itemForm";
         }
-        return "redirect:http://localhost:8080/admin/items";
+        Pageable pageable = PageRequest.of(page.isPresent() ? page.get() : 0, 3);//
+        // PageRequest - Data jpa에서 사용하는 페이지 요청객체
+        // of() 메서드를 사용하여 페이지 번화와 페이지당 항목수 지정하여 페이지 요청 정보생성
+        // 0 은 url 경로에서 받아온 페이지 번호를 확인하고 , 값이 없으면 0 = 보고 있던 페이지가 없으면 첫번째 페이지를 보여줌
+        // 3 은 한 페이지 당 보여줄 항목수
+
+
+        Page<Item> items = itemService.getAdminItemPage(itemSearchDto, pageable);
+        //itemSearchDto를 사용하여 페이지 네이션 된 테이터를 조회
+        model.addAttribute("items", items); //조회된 페이지 네이션된 데이터를 모델에 추가
+        model.addAttribute("itemSearchDto", itemSearchDto); //검색 조건 모델에 추가
+        model.addAttribute("maxPage", 5);
+        return "item/itemMng";
     }
 
-    @GetMapping({"/admin/items","admin/items/{page}"})
+    @GetMapping(value = {"/admin/items","admin/items/{page}"})
     public String itemManage(ItemSearchDto itemSearchDto, @PathVariable("page") Optional<Integer> page, Model model){
 
         Pageable pageable = PageRequest.of(page.isPresent() ? page.get() : 0, 3);//
