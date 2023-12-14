e5e2478b9bb4f72b89411071b68ef0b663ff3b65
donghan529 <dong529@naver.com>
2023-12-14 22:47:35 +0900

    1214
    더미데이터

--- projectCoffee/src/main/resources/data.sql
+++ projectCoffee/src/main/resources/data.sql
@@ -1,102 +1,102 @@
 -- etc
 INSERT INTO item (item_id, reg_time, update_time, created_by, modified_by, item_category, item_detail, item_nm, item_sell_status, price, stock_number)
-VALUES (1, '2023-12-13 15:50:19.335193', '2023-12-13 15:50:19.335193', 'dong529@naver.com', 'dong529@naver.com', 'etc', '신선한 맛과 향을 고스란히 다음 수제청', '수제과일청 고농축 레몬청 1kg', 'SELL', 11900, 100);
+VALUES (1, '2023-12-13 15:50:19.335193', '2023-12-13 15:50:19.335193', 'dong529@naver.com', 'dong529@naver.com', 'etc', '신선한 맛과 향을 고스란히 담은 수제청', '수제과일청 고농축 청1', 'SELL', 11900, 100);
 INSERT INTO item_img (item_img_id, img_name, ori_img_name, img_url, rep_img_yn, item_id)
-VALUES (1, '수제청1.jpg', '/images/item/수제청1.jpg', '수제청1.jpg', 'Y', 1), (2, '수제청2.jpg', '/images/item/수제청2.jpg', '수제청2.jpg', 'N', 1);
+VALUES (1, 'etc1.jpg', '/images/item/etc1.jpg', 'etc1.jpg', 'Y', 1), (2, 'etc2.jpg', '/images/item/etc2.jpg', 'etc2.jpg', 'N', 1);
 
 INSERT INTO item (item_id, reg_time, update_time, created_by, modified_by, item_category, item_detail, item_nm, item_sell_status, price, stock_number)
-VALUES (2, '2023-12-13 15:50:19.335193', '2023-12-13 15:50:19.335193', 'dong529@naver.com', 'dong529@naver.com', 'etc', '신선한 맛과 향을 고스란히 다음 수제청', '수제과일청 고농축 딸기청 1kg', 'SELL', 11900, 100);
+VALUES (2, '2023-12-13 15:50:19.335193', '2023-12-13 15:50:19.335193', 'dong529@naver.com', 'dong529@naver.com', 'etc', '신선한 맛과 향을 고스란히 담은 수제청', '수제과일청 고농축 청2', 'SELL', 11900, 100);
 INSERT INTO item_img (item_img_id, img_name, ori_img_name, img_url, rep_img_yn, item_id)
-VALUES (3, '수제청3.jpg', '/images/item/3.jpg', '수제청3.jpg', 'Y', 2), (4, '수제청4.jpg', '/images/item/4.jpg', '수제청4.jpg', 'N', 2);
+VALUES (3, 'etc3.jpg', '/images/item/3.jpg', 'etc3.jpg', 'Y', 2), (4, 'etc4.jpg', '/images/item/4.jpg', 'etc4.jpg', 'N', 2);
 
 INSERT INTO item (item_id, reg_time, update_time, created_by, modified_by, item_category, item_detail, item_nm, item_sell_status, price, stock_number)
-VALUES (3, '2023-12-13 15:50:19.335193', '2023-12-13 15:50:19.335193', 'dong529@naver.com', 'dong529@naver.com', 'etc', '신선한 맛과 향을 고스란히 다음 수제청', '수제과일청 고농축 딸기청 1kg', 'SELL', 11900, 100);
+VALUES (3, '2023-12-13 15:50:19.335193', '2023-12-13 15:50:19.335193', 'dong529@naver.com', 'dong529@naver.com', 'etc', '신선한 맛과 향을 고스란히 담은 수제청', '수제과일청 고농축 청3', 'SELL', 11900, 100);
 INSERT INTO item_img (item_img_id, img_name, ori_img_name, img_url, rep_img_yn, item_id)
-VALUES (5, '수제청5.jpg', '/images/item/5.jpg', '수제청5.jpg', 'Y', 3), (6, '수제청6.jpg', '/images/item/6.jpg', '수제청6.jpg', 'N', 3);
+VALUES (5, 'etc5.jpg', '/images/item/5.jpg', 'etc5.jpg', 'Y', 3), (6, 'etc6.jpg', '/images/item/6.jpg', 'etc6.jpg', 'N', 3);
 
 INSERT INTO item (item_id, reg_time, update_time, created_by, modified_by, item_category, item_detail, item_nm, item_sell_status, price, stock_number)
-VALUES (4, '2023-12-13 15:50:19.335193', '2023-12-13 15:50:19.335193', 'dong529@naver.com', 'dong529@naver.com', 'etc', '신선한 맛과 향을 고스란히 다음 수제청', '수제과일청 고농축 딸기청 1kg', 'SELL', 11900, 100);
+VALUES (4, '2023-12-13 15:50:19.335193', '2023-12-13 15:50:19.335193', 'dong529@naver.com', 'dong529@naver.com', 'etc', '신선한 맛과 향을 고스란히 담은 수제청', '수제과일청 고농축 청4', 'SELL', 11900, 100);
 INSERT INTO item_img (item_img_id, img_name, ori_img_name, img_url, rep_img_yn, item_id)
-VALUES (7, '수제청7.jpg', '/images/item/7.jpg', '수제청7.jpg', 'Y', 4), (8, '수제청8.jpg', '/images/item/8.jpg', '수제청8.jpg', 'N', 4);
+VALUES (7, 'etc7.jpg', '/images/item/7.jpg', 'etc7.jpg', 'Y', 4), (8, 'etc8.jpg', '/images/item/8.jpg', 'etc8.jpg', 'N', 4);
 
 INSERT INTO item (item_id, reg_time, update_time, created_by, modified_by, item_category, item_detail, item_nm, item_sell_status, price, stock_number)
-VALUES (5, '2023-12-13 15:50:19.335193', '2023-12-13 15:50:19.335193', 'dong529@naver.com', 'dong529@naver.com', 'etc', '신선한 맛과 향을 고스란히 다음 수제청', '수제과일청 고농축 딸기청 1kg', 'SELL', 11900, 100);
+VALUES (5, '2023-12-13 15:50:19.335193', '2023-12-13 15:50:19.335193', 'dong529@naver.com', 'dong529@naver.com', 'etc', '신선한 맛과 향을 고스란히 담은 수제청', '수제과일청 고농축 청5', 'SELL', 11900, 100);
 INSERT INTO item_img (item_img_id, img_name, ori_img_name, img_url, rep_img_yn, item_id)
-VALUES (9, '수제청9.jpg', '/images/item/9.jpg', '수제청9.jpg', 'Y', 5), (10, '수제청10.jpg', '/images/item/10.jpg', '수제청10.jpg', 'N', 5);
+VALUES (9, 'etc9.jpg', '/images/item/9.jpg', 'etc9.jpg', 'Y', 5), (10, 'etc10.jpg', '/images/item/10.jpg', 'etc10.jpg', 'N', 5);
 
 INSERT INTO item (item_id, reg_time, update_time, created_by, modified_by, item_category, item_detail, item_nm, item_sell_status, price, stock_number)
-VALUES (6, '2023-12-13 15:50:19.335193', '2023-12-13 15:50:19.335193', 'dong529@naver.com', 'dong529@naver.com', 'etc', '신선한 맛과 향을 고스란히 다음 수제청', '수제과일청 고농축 딸기청 1kg', 'SELL', 11900, 100);
+VALUES (6, '2023-12-13 15:50:19.335193', '2023-12-13 15:50:19.335193', 'dong529@naver.com', 'dong529@naver.com', 'etc', '신선한 맛과 향을 고스란히 담은 수제청', '수제과일청 고농축 청6', 'SELL', 11900, 100);
 INSERT INTO item_img (item_img_id, img_name, ori_img_name, img_url, rep_img_yn, item_id)
-VALUES (11, '수제청11.jpg', '/images/item/11.jpg', '수제청11.jpg', 'Y', 6), (12, '수제청12.jpg', '/images/item/12.jpg', '수제청12.jpg', 'N', 6);
+VALUES (11, 'etc11.jpg', '/images/item/11.jpg', 'etc11.jpg', 'Y', 6), (12, 'etc12.jpg', '/images/item/12.jpg', 'etc12.jpg', 'N', 6);
 
 INSERT INTO item (item_id, reg_time, update_time, created_by, modified_by, item_category, item_detail, item_nm, item_sell_status, price, stock_number)
-VALUES (7, '2023-12-13 15:50:19.335193', '2023-12-13 15:50:19.335193', 'dong529@naver.com', 'dong529@naver.com', 'etc', '신선한 맛과 향을 고스란히 다음 수제청', '수제과일청 고농축 딸기청 1kg', 'SELL', 11900, 100);
+VALUES (7, '2023-12-13 15:50:19.335193', '2023-12-13 15:50:19.335193', 'dong529@naver.com', 'dong529@naver.com', 'etc', '신선한 맛과 향을 고스란히 담은 수제청', '수제과일청 고농축 청7', 'SELL', 11900, 100);
 INSERT INTO item_img (item_img_id, img_name, ori_img_name, img_url, rep_img_yn, item_id)
-VALUES (13, '수제청13.jpg', '/images/item/13.jpg', '수제청13.jpg', 'Y', 7), (14, '수제청14.jpg', '/images/item/14.jpg', '수제청14.jpg', 'N', 7);
+VALUES (13, 'etc13.jpg', '/images/item/13.jpg', 'etc13.jpg', 'Y', 7), (14, 'etc14.jpg', '/images/item/14.jpg', 'etc14.jpg', 'N', 7);
 
 INSERT INTO item (item_id, reg_time, update_time, created_by, modified_by, item_category, item_detail, item_nm, item_sell_status, price, stock_number)
-VALUES (8, '2023-12-13 15:50:19.335193', '2023-12-13 15:50:19.335193', 'dong529@naver.com', 'dong529@naver.com', 'etc', '신선한 맛과 향을 고스란히 다음 수제청', '수제과일청 고농축 딸기청 1kg', 'SELL', 11900, 100);
+VALUES (8, '2023-12-13 15:50:19.335193', '2023-12-13 15:50:19.335193', 'dong529@naver.com', 'dong529@naver.com', 'etc', '신선한 맛과 향을 고스란히 담은 수제청', '수제과일청 고농축 청8', 'SELL', 11900, 100);
 INSERT INTO item_img (item_img_id, img_name, ori_img_name, img_url, rep_img_yn, item_id)
-VALUES (15, '수제청15.jpg', '/images/item/15.jpg', '수제청15.jpg', 'Y', 8), (16, '수제청16.jpg', '/images/item/16.jpg', '수제청16.jpg', 'N', 8);
+VALUES (15, 'etc15.jpg', '/images/item/15.jpg', 'etc15.jpg', 'Y', 8), (16, 'etc16.jpg', '/images/item/16.jpg', 'etc16.jpg', 'N', 8);
 
 INSERT INTO item (item_id, reg_time, update_time, created_by, modified_by, item_category, item_detail, item_nm, item_sell_status, price, stock_number)
-VALUES (9, '2023-12-13 15:50:19.335193', '2023-12-13 15:50:19.335193', 'dong529@naver.com', 'dong529@naver.com', 'etc', '신선한 맛과 향을 고스란히 다음 수제청', '수제과일청 고농축 딸기청 1kg', 'SELL', 11900, 100);
+VALUES (9, '2023-12-13 15:50:19.335193', '2023-12-13 15:50:19.335193', 'dong529@naver.com', 'dong529@naver.com', 'etc', '신선한 맛과 향을 고스란히 담은 수제청', '수제과일청 고농축 청9', 'SELL', 11900, 100);
 INSERT INTO item_img (item_img_id, img_name, ori_img_name, img_url, rep_img_yn, item_id)
-VALUES (17, '수제청17.jpg', '/images/item/17.jpg', '수제청17.jpg', 'Y', 9), (18, '수제청18.jpg', '/images/item/18.jpg', '수제청18.jpg', 'N', 9);
+VALUES (17, 'etc17.jpg', '/images/item/17.jpg', 'etc17.jpg', 'Y', 9), (18, 'etc18.jpg', '/images/item/18.jpg', 'etc18.jpg', 'N', 9);
 
 INSERT INTO item (item_id, reg_time, update_time, created_by, modified_by, item_category, item_detail, item_nm, item_sell_status, price, stock_number)
-VALUES (10, '2023-12-13 15:50:19.335193', '2023-12-13 15:50:19.335193', 'dong529@naver.com', 'dong529@naver.com', 'etc', '신선한 맛과 향을 고스란히 다음 수제청', '수제과일청 고농축 딸기청 1kg', 'SELL', 11900, 100);
+VALUES (10, '2023-12-13 15:50:19.335193', '2023-12-13 15:50:19.335193', 'dong529@naver.com', 'dong529@naver.com', 'etc', '신선한 맛과 향을 고스란히 담은 수제청', '수제과일청 고농축 청10', 'SELL', 11900, 100);
 INSERT INTO item_img (item_img_id, img_name, ori_img_name, img_url, rep_img_yn, item_id)
-VALUES (19, '수제청19.jpg', '/images/item/19.jpg', '수제청19.jpg', 'Y', 10), (20, '수제청20.jpg', '/images/item/20.jpg', '수제청20.jpg', 'N', 10);
+VALUES (19, 'etc19.jpg', '/images/item/19.jpg', 'etc19.jpg', 'Y', 10), (20, 'etc20.jpg', '/images/item/20.jpg', 'etc20.jpg', 'N', 10);
 
 -- tool
 
 INSERT INTO item (item_id, reg_time, update_time, created_by, modified_by, item_category, item_detail, item_nm, item_sell_status, price, stock_number)
-VALUES (11, '2023-12-13 15:50:19.335193', '2023-12-13 15:50:19.335193', 'dong529@naver.com', 'dong529@naver.com', 'etc', '신선한 맛과 향을 고스란히 다음 수제청', '수제과일청 고농축 레몬청 1kg', 'SELL', 11900, 100);
+VALUES (11, '2023-12-13 15:50:19.335193', '2023-12-13 15:50:19.335193', 'dong529@naver.com', 'dong529@naver.com', 'tools', '클래식하고 고급스러운', '커피용품1', 'SELL', 11900, 100);
 INSERT INTO item_img (item_img_id, img_name, ori_img_name, img_url, rep_img_yn, item_id)
-VALUES (21, '수제청1.jpg', '/images/item/수제청1.jpg', '수제청1.jpg', 'Y', 11), (22, '수제청2.jpg', '/images/item/수제청2.jpg', '수제청2.jpg', 'N', 11);
+VALUES (21, 'tool1.jpg', '/images/item/tool1.jpg', 'tool1.jpg', 'Y', 11), (22, 'tool2.jpg', '/images/item/tool2.jpg', 'tool2.jpg', 'N', 11);
 
 INSERT INTO item (item_id, reg_time, update_time, created_by, modified_by, item_category, item_detail, item_nm, item_sell_status, price, stock_number)
-VALUES (12, '2023-12-13 15:50:19.335193', '2023-12-13 15:50:19.335193', 'dong529@naver.com', 'dong529@naver.com', 'etc', '신선한 맛과 향을 고스란히 다음 수제청', '수제과일청 고농축 딸기청 1kg', 'SELL', 11900, 100);
+VALUES (12, '2023-12-13 15:50:19.335193', '2023-12-13 15:50:19.335193', 'dong529@naver.com', 'dong529@naver.com', 'tools', '클래식하고 고급스러운', '커피용품2', 'SELL', 11900, 100);
 INSERT INTO item_img (item_img_id, img_name, ori_img_name, img_url, rep_img_yn, item_id)
-VALUES (23, '수제청3.jpg', '/images/item/3.jpg', '수제청3.jpg', 'Y', 12), (24, '수제청4.jpg', '/images/item/4.jpg', '수제청4.jpg', 'N', 12);
+VALUES (23, 'tool3.jpg', '/images/item/3.jpg', 'tool3.jpg', 'Y', 12), (24, 'tool4.jpg', '/images/item/4.jpg', 'tool4.jpg', 'N', 12);
 
 INSERT INTO item (item_id, reg_time, update_time, created_by, modified_by, item_category, item_detail, item_nm, item_sell_status, price, stock_number)
-VALUES (13, '2023-12-13 15:50:19.335193', '2023-12-13 15:50:19.335193', 'dong529@naver.com', 'dong529@naver.com', 'etc', '신선한 맛과 향을 고스란히 다음 수제청', '수제과일청 고농축 딸기청 1kg', 'SELL', 11900, 100);
+VALUES (13, '2023-12-13 15:50:19.335193', '2023-12-13 15:50:19.335193', 'dong529@naver.com', 'dong529@naver.com', 'tools', '클래식하고 고급스러운', '커피용품3', 'SELL', 11900, 100);
 INSERT INTO item_img (item_img_id, img_name, ori_img_name, img_url, rep_img_yn, item_id)
-VALUES (25, '수제청5.jpg', '/images/item/5.jpg', '수제청5.jpg', 'Y', 13), (26, '수제청6.jpg', '/images/item/6.jpg', '수제청6.jpg', 'N', 13);
+VALUES (25, 'tool5.jpg', '/images/item/5.jpg', 'tool5.jpg', 'Y', 13), (26, 'tool6.jpg', '/images/item/6.jpg', 'tool6.jpg', 'N', 13);
 
 INSERT INTO item (item_id, reg_time, update_time, created_by, modified_by, item_category, item_detail, item_nm, item_sell_status, price, stock_number)
-VALUES (14, '2023-12-13 15:50:19.335193', '2023-12-13 15:50:19.335193', 'dong529@naver.com', 'dong529@naver.com', 'etc', '신선한 맛과 향을 고스란히 다음 수제청', '수제과일청 고농축 딸기청 1kg', 'SELL', 11900, 100);
+VALUES (14, '2023-12-13 15:50:19.335193', '2023-12-13 15:50:19.335193', 'dong529@naver.com', 'dong529@naver.com', 'tools', '클래식하고 고급스러운', '커피용품4', 'SELL', 11900, 100);
 INSERT INTO item_img (item_img_id, img_name, ori_img_name, img_url, rep_img_yn, item_id)
-VALUES (27, '수제청7.jpg', '/images/item/7.jpg', '수제청7.jpg', 'Y', 14), (28, '수제청8.jpg', '/images/item/8.jpg', '수제청8.jpg', 'N', 14);
+VALUES (27, 'tool7.jpg', '/images/item/7.jpg', 'tool7.jpg', 'Y', 14), (28, 'tool8.jpg', '/images/item/8.jpg', 'tool8.jpg', 'N', 14);
 
 INSERT INTO item (item_id, reg_time, update_time, created_by, modified_by, item_category, item_detail, item_nm, item_sell_status, price, stock_number)
-VALUES (15, '2023-12-13 15:50:19.335193', '2023-12-13 15:50:19.335193', 'dong529@naver.com', 'dong529@naver.com', 'etc', '신선한 맛과 향을 고스란히 다음 수제청', '수제과일청 고농축 딸기청 1kg', 'SELL', 11900, 100);
+VALUES (15, '2023-12-13 15:50:19.335193', '2023-12-13 15:50:19.335193', 'dong529@naver.com', 'dong529@naver.com', 'tools', '클래식하고 고급스러운', '커피용품5', 'SELL', 11900, 100);
 INSERT INTO item_img (item_img_id, img_name, ori_img_name, img_url, rep_img_yn, item_id)
-VALUES (29, '수제청9.jpg', '/images/item/9.jpg', '수제청9.jpg', 'Y', 15), (30, '수제청10.jpg', '/images/item/10.jpg', '수제청10.jpg', 'N', 15);
+VALUES (29, 'tool9.jpg', '/images/item/9.jpg', 'tool9.jpg', 'Y', 15), (30, 'tool10.jpg', '/images/item/10.jpg', 'tool10.jpg', 'N', 15);
 
 INSERT INTO item (item_id, reg_time, update_time, created_by, modified_by, item_category, item_detail, item_nm, item_sell_status, price, stock_number)
-VALUES (16, '2023-12-13 15:50:19.335193', '2023-12-13 15:50:19.335193', 'dong529@naver.com', 'dong529@naver.com', 'etc', '신선한 맛과 향을 고스란히 다음 수제청', '수제과일청 고농축 딸기청 1kg', 'SELL', 11900, 100);
+VALUES (16, '2023-12-13 15:50:19.335193', '2023-12-13 15:50:19.335193', 'dong529@naver.com', 'dong529@naver.com', 'tools', '클래식하고 고급스러운', '커피용품6', 'SELL', 11900, 100);
 INSERT INTO item_img (item_img_id, img_name, ori_img_name, img_url, rep_img_yn, item_id)
-VALUES (31, '수제청11.jpg', '/images/item/11.jpg', '수제청11.jpg', 'Y', 16), (32, '수제청12.jpg', '/images/item/12.jpg', '수제청12.jpg', 'N', 16);
+VALUES (31, 'tool11.jpg', '/images/item/11.jpg', 'tool11.jpg', 'Y', 16), (32, 'tool12.jpg', '/images/item/12.jpg', 'tool12.jpg', 'N', 16);
 
 INSERT INTO item (item_id, reg_time, update_time, created_by, modified_by, item_category, item_detail, item_nm, item_sell_status, price, stock_number)
-VALUES (17, '2023-12-13 15:50:19.335193', '2023-12-13 15:50:19.335193', 'dong529@naver.com', 'dong529@naver.com', 'etc', '신선한 맛과 향을 고스란히 다음 수제청', '수제과일청 고농축 딸기청 1kg', 'SELL', 11900, 100);
+VALUES (17, '2023-12-13 15:50:19.335193', '2023-12-13 15:50:19.335193', 'dong529@naver.com', 'dong529@naver.com', 'tools', '클래식하고 고급스러운', '커피용품7', 'SELL', 11900, 100);
 INSERT INTO item_img (item_img_id, img_name, ori_img_name, img_url, rep_img_yn, item_id)
-VALUES (33, '수제청13.jpg', '/images/item/13.jpg', '수제청13.jpg', 'Y', 17), (34, '수제청14.jpg', '/images/item/14.jpg', '수제청14.jpg', 'N', 17);
+VALUES (33, 'tool13.jpg', '/images/item/13.jpg', 'tool13.jpg', 'Y', 17), (34, 'tool14.jpg', '/images/item/14.jpg', 'tool14.jpg', 'N', 17);
 
 INSERT INTO item (item_id, reg_time, update_time, created_by, modified_by, item_category, item_detail, item_nm, item_sell_status, price, stock_number)
-VALUES (18, '2023-12-13 15:50:19.335193', '2023-12-13 15:50:19.335193', 'dong529@naver.com', 'dong529@naver.com', 'etc', '신선한 맛과 향을 고스란히 다음 수제청', '수제과일청 고농축 딸기청 1kg', 'SELL', 11900, 100);
+VALUES (18, '2023-12-13 15:50:19.335193', '2023-12-13 15:50:19.335193', 'dong529@naver.com', 'dong529@naver.com', 'tools', '클래식하고 고급스러운', '커피용품8', 'SELL', 11900, 100);
 INSERT INTO item_img (item_img_id, img_name, ori_img_name, img_url, rep_img_yn, item_id)
-VALUES (35, '수제청15.jpg', '/images/item/15.jpg', '수제청15.jpg', 'Y', 18), (36, '수제청16.jpg', '/images/item/16.jpg', '수제청16.jpg', 'N', 18);
+VALUES (35, 'tool15.jpg', '/images/item/15.jpg', 'tool15.jpg', 'Y', 18), (36, 'tool16.jpg', '/images/item/16.jpg', 'tool16.jpg', 'N', 18);
 
 INSERT INTO item (item_id, reg_time, update_time, created_by, modified_by, item_category, item_detail, item_nm, item_sell_status, price, stock_number)
-VALUES (19, '2023-12-13 15:50:19.335193', '2023-12-13 15:50:19.335193', 'dong529@naver.com', 'dong529@naver.com', 'etc', '신선한 맛과 향을 고스란히 다음 수제청', '수제과일청 고농축 딸기청 1kg', 'SELL', 11900, 100);
+VALUES (19, '2023-12-13 15:50:19.335193', '2023-12-13 15:50:19.335193', 'dong529@naver.com', 'dong529@naver.com', 'tools', '클래식하고 고급스러운', '커피용품9', 'SELL', 11900, 100);
 INSERT INTO item_img (item_img_id, img_name, ori_img_name, img_url, rep_img_yn, item_id)
-VALUES (37, '수제청17.jpg', '/images/item/17.jpg', '수제청17.jpg', 'Y', 19), (38, '수제청18.jpg', '/images/item/18.jpg', '수제청18.jpg', 'N', 19);
+VALUES (37, 'tool17.jpg', '/images/item/17.jpg', 'tool17.jpg', 'Y', 19), (38, 'tool18.jpg', '/images/item/18.jpg', 'tool18.jpg', 'N', 19);
 
 INSERT INTO item (item_id, reg_time, update_time, created_by, modified_by, item_category, item_detail, item_nm, item_sell_status, price, stock_number)
-VALUES (20, '2023-12-13 15:50:19.335193', '2023-12-13 15:50:19.335193', 'dong529@naver.com', 'dong529@naver.com', 'etc', '신선한 맛과 향을 고스란히 다음 수제청', '수제과일청 고농축 딸기청 1kg', 'SELL', 11900, 100);
+VALUES (20, '2023-12-13 15:50:19.335193', '2023-12-13 15:50:19.335193', 'dong529@naver.com', 'dong529@naver.com', 'tools', '클래식하고 고급스러운', '커피용품10', 'SELL', 11900, 100);
 INSERT INTO item_img (item_img_id, img_name, ori_img_name, img_url, rep_img_yn, item_id)
-VALUES (39, '수제청19.jpg', '/images/item/19.jpg', '수제청19.jpg', 'Y', 20), (40, '수제청20.jpg', '/images/item/20.jpg', '수제청20.jpg', 'N', 20);
+VALUES (39, 'tool19.jpg', '/images/item/19.jpg', 'tool19.jpg', 'Y', 20), (40, 'tool20.jpg', '/images/item/20.jpg', 'tool20.jpg', 'N', 20);
